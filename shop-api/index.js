0f63c28d04f9ccaf1b8b18840ac2323701e7f667
infomagnum159 <infomagnum159@gmail.com>
2022-08-18 16:42:45 +0600

    Made the backend part

---
+++ shop-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 8000;
+
+app.post('/encode', (req, res) => {
+    res.send({ "encoded": Vigenere.Cipher(req.body.password).crypt(req.body.encode) });
+});
+
+app.post('/decode', (req, res) => {
+    res.send({ "decoded": Vigenere.Decipher(req.body.password).crypt(req.body.decode) });
+});
+
+app.listen(port, () => {
+    console.log('Server started on http:localhost:' + port)
+});
